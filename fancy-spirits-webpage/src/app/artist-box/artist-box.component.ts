9757161904ad996b3c7af3f39e8c6132c75b1368
Konrad Koschel <konrad.koschel@hotmail.de>
2022-06-16 01:04:50 +0200

    Make Artist Box dynamic

--- fancy-spirits-webpage/src/app/artist-box/artist-box.component.ts
+++ fancy-spirits-webpage/src/app/artist-box/artist-box.component.ts
@@ -1,16 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit, ViewEncapsulation } from '@angular/core';
 import { faSpotify, faItunes, faYoutube, faSoundcloud } from '@fortawesome/free-brands-svg-icons';
 
 @Component({
   selector: 'app-artist-box',
   templateUrl: './artist-box.component.html',
-  styleUrls: ['./artist-box.component.scss']
+  styleUrls: ['./artist-box.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class ArtistBoxComponent implements OnInit {
   faSpotify = faSpotify
   faItunes = faItunes
   faYoutube = faYoutube
   faSoundcloud = faSoundcloud
+
+  @Input()
+  artistImage: string = "";
+
+  @Input()
+  artistName: string = "";
+
+  @Input()
+  biography: string = "";
+
+  @Input()
+  genres: string[] = [];
 
   constructor() { }
 
--- fancy-spirits-webpage/src/app/artist-page/artist-page.component.ts
+++ fancy-spirits-webpage/src/app/artist-page/artist-page.component.ts
@@ -6,6 +6,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./artist-page.component.scss']
 })
 export class ArtistPageComponent implements OnInit {
+  artistName = "doxbleK";
+  artistPicture = "../../assets/doxblek.jpeg";
+  biography = `<p>doxbleK is a world-wide known versatile artist mostly known for his Drum & Bass releases. Together with Jon Kat he majorly influenced that genre...</p>
+  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias mollitia pariatur illum voluptates labore consectetur ex beatae rem deleniti, ut suscipit sunt doloribus. Quam beatae rem nulla ullam minima debitis.
+  </p>`;
+  genres = ["Drum & Bass", "House", "Electro"];
 
   constructor() { }
 
