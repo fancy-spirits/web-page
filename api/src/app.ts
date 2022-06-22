e6c7a6d88e4a8f3c50f03f09c786772eb3aaa79a
Konrad Koschel <konrad.koschel@hotmail.de>
2022-06-22 16:35:05 +0200

    Fixed Maximum Body Size for API

--- api/src/app.ts
+++ api/src/app.ts
@@ -8,7 +8,7 @@ import {default as deleteRoutes} from "./routes/delete";
 const app = express();
 const PORT = process.env.PORT ?? 4000;
 
-app.use(express.json());
+app.use(express.json({limit: "50mb"}));
 app.use(express.urlencoded({extended: true}));
 app.use(cors());
 app.use(morgan("combined"));
