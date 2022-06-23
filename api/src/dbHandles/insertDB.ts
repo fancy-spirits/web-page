84c432e243468a1a295b0f0c2baf14130596c1e9
Konrad Koschel <konrad.koschel@hotmail.de>
2022-06-23 22:48:11 +0200

    Return inserted record

--- api/src/dbHandles/insertDB.ts
+++ api/src/dbHandles/insertDB.ts
@@ -9,14 +9,14 @@ export async function createArtist(artist: Artist) {
         pwd_hash: "x",
         salt: "x"
     });
-    const insertStatement = `INSERT INTO artists (name, picture, biography, user) VALUES ($1, $2, $3, $4)`;
+    const insertStatement = `INSERT INTO artists (name, picture, biography, user) VALUES ($1, $2, $3, $4) RETURNING *`;
     const created: Artist = await (await db.querySingle(insertStatement, [artist.name, artist.picture, artist.biography, artistUser.id!])).rows[0];
     return created;
 }
 
 export async function createRelease(release: Release, artists: Artist[]) {
-    const insertStatementRelease = `INSERT INTO releases (name, release_date, release_type, artwork, description) VALUES ($1, $2, $3, $4, $5)`;
-    const insertStatementReleaseContribution = `INSERT INTO release_contribution (artist, release) VALUES ($1, $2)`;
+    const insertStatementRelease = `INSERT INTO releases (name, release_date, release_type, artwork, description) VALUES ($1, $2, $3, $4, $5) RETURNING *`;
+    const insertStatementReleaseContribution = `INSERT INTO release_contribution (artist, release) VALUES ($1, $2) RETURNING *`;
     const result = await db.querySingle(insertStatementRelease, [release.name, release.release_date, release.release_type, release.artwork, release.description]);
     const createdRelease = result.rows[0];
     const queries: [text: string, params: any[]][] = artists.map(artist => [insertStatementReleaseContribution, [artist["id"], createdRelease["_id"]]]);
@@ -27,7 +27,7 @@ export async function createRelease(release: Release, artists: Artist[]) {
 }
 
 export async function createSocialLinks(socialLinks: SocialLink[], artist: Artist & {id: string}) {
-    const insertStatementSocialLink = `INSERT INTO social_link (platform, link, platform_type, artist) VALUES ($1, $2, $3, $4)`;
+    const insertStatementSocialLink = `INSERT INTO social_link (platform, link, platform_type, artist) VALUES ($1, $2, $3, $4) RETURNING *`;
     const queries: [text: string, params: any[]][] = socialLinks.map(socialLink => [insertStatementSocialLink,  [socialLink.platform, socialLink.link, socialLink.platform_type, artist.id]]);
     const results = await db.queryMultiple([
         ...queries
@@ -40,7 +40,7 @@ export async function createArtistUser(user: Partial<User>) {
     if (!user.privateMail || !user.pwd_hash || !user.salt) {
         throw "Invalid User";
     }
-    const insertStatementUser = `INSERT INTO users (private_mail, pwd_hash, salt, role) VALUES ($1, $2, $3, $4)`;
+    const insertStatementUser = `INSERT INTO users (private_mail, pwd_hash, salt, role) VALUES ($1, $2, $3, $4) RETURNING *`;
     const userResult = await db.querySingle(insertStatementUser, [user.privateMail, user.pwd_hash, user.salt, "artist"]);
     const createdUser: User = userResult.rows[0];
     console.log(userResult);
@@ -49,7 +49,7 @@ export async function createArtistUser(user: Partial<User>) {
 }
 
 export async function createGenre(genre: Genre) {
-    const insertStatementGenre = `INSERT INTO genre (name) VALUES ($1)`;
+    const insertStatementGenre = `INSERT INTO genre (name) VALUES ($1) RETURNING *`;
     const genreResult = await db.querySingle(insertStatementGenre, [genre.name]);
     const createdGenre: Genre = genreResult.rows[0];
     console.log(`Created artist ${createdGenre}`);
