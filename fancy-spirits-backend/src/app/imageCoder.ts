c14bfe42006ea3b680a1539b13401867b539d565
Konrad Koschel <konrad.koschel@hotmail.de>
2022-06-28 17:19:05 +0200

    Try'na fixing the image

--- fancy-spirits-backend/src/app/imageCoder.ts
+++ fancy-spirits-backend/src/app/imageCoder.ts
@@ -9,17 +9,19 @@ export function toBuffer(base64: string) {
     return buffer;
 }
 
-export function toBase64(buffer: ArrayBuffer) {
-    console.log("buffertype: ", typeof buffer, Object.keys(buffer));
-    const byteArray = new Uint8Array(buffer) as any;
-    // const imageB64 = base64.bytesToBase64(byteArray);
-    let imageB64_coded = "";
+export function toBase64(buffer: any) {
+    console.log("paramtype: ", typeof buffer, Object.keys(buffer));
+    console.log("bufferType: ", typeof buffer.data);
     
-    for (let i = 0; i < byteArray.data.length; i++) {
-        imageB64_coded += String.fromCharCode(byteArray.data[i]);
-    }
-    console.log("Img64_coded: ", imageB64_coded);
-    const imageB64 =  window.btoa(imageB64_coded);
-    console.log("Img64: ", imageB64);
-    return `data:image/jpeg;charset=utf-8;base64,${imageB64}`;
+    // const byteArray = new Uint8Array(buffer) as any;
+    // // const imageB64 = base64.bytesToBase64(byteArray);
+    // let imageB64_coded = "";
+    
+    // for (let i = 0; i < byteArray.data.length; i++) {
+    //     imageB64_coded += String.fromCharCode(byteArray.data[i]);
+    // }
+    // console.log("Img64_coded: ", imageB64_coded);
+    // const imageB64 =  window.btoa(imageB64_coded);
+    // console.log("Img64: ", imageB64);
+    return `data:image/jpeg;charset=utf-8;base64,${/* imageB64 */ null}`;
 }
