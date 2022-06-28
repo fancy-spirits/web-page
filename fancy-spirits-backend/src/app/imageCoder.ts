1592fec51291b793bbc786392cdf26c99692cd2e
Konrad Koschel <konrad.koschel@hotmail.de>
2022-06-28 16:58:48 +0200

    Try'na fixing the image

--- fancy-spirits-backend/src/app/imageCoder.ts
+++ fancy-spirits-backend/src/app/imageCoder.ts
@@ -9,20 +9,16 @@ export function toBuffer(base64: string) {
     return buffer;
 }
 
-export function toBase64(buffer: Uint8Array | ArrayBuffer) {
-    console.log("Buffer: ", buffer);
-    
-    const byteArray = buffer instanceof ArrayBuffer ? new Uint8Array(buffer) : buffer;
+export function toBase64(buffer: ArrayBuffer) {
+    const byteArray = new Uint8Array(buffer) as any;
     console.log("Bytearray: ", byteArray);
     // const imageB64 = base64.bytesToBase64(byteArray);
     let imageB64_coded = "";
     console.log("Buffer Lengths: ", /* buffer.length, */ buffer.byteLength);
     console.log("Bytearray Lengths: ", byteArray.length, byteArray.byteLength);
     
-    for (let i = 0; i < byteArray.length; i++) {
-        imageB64_coded += byteArray[i];
-        console.log(byteArray[i], String.fromCharCode(byteArray[i]));
-        
+    for (let i = 0; i < byteArray.data.length; i++) {
+        imageB64_coded += String.fromCharCode(byteArray.data[i]);
     }
     console.log("Img64_coded: ", imageB64_coded);
     const imageB64 =  window.btoa(imageB64_coded);
